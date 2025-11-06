d5e7d26164f92113f5b77e446f9287da4eabd0ee
fedecorra11 <fede.corradi2011@gmail.com>
2025-11-06 21:42:35 +0000

    esercizio 5.c

---
+++ esercizio5.c
@@ -0,0 +1,32 @@
+
+
+#include <stdio.h> 
+
+intmain(){
+    int a, b, c, temp;
+    printf("inserisci il primo numero intero");
+    scanf(%d, &a);
+    printf("inserisci il secondo numero intero");
+    scanf(%d, &b);
+    printf("inserisci il terzo numero intero");
+    scanf(&d, &c);
+
+    printf("questi sono i valori prima dello scambio delle variabili");
+    printf("a= %d, a");
+    printf("b= %d, b");
+    printf("c= %d, c");
+
+    temp=b;
+    b=c;
+    c=a,
+    a=temp;
+
+    printf("questi sono i valori dopo lo scambio delle variabili")
+    printf("a=&d", a)
+    printf("b=&d", b)
+    printf("c=&d", c)
+
+    return0;
+
+    
+}
